0ecba5b4918d11ffce203315f021478b3b599637
Newbiekk <newbiekk1126@gmail.com>
2024-10-21 23:03:02 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for (int i=0 ; i<7 ; i++) {
+            answer += n%10;
+            n = n/10;
+        }
+        
+        return answer ;
+    }
+}
