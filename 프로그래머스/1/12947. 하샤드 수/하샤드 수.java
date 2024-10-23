c93854ecf098da99dfa084a81fef5c8b8b7a1999
Newbiekk <newbiekk1126@gmail.com>
2024-10-23 09:11:47 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum = 0;
+        int tempNum = x;
+        
+        for (int i = 0 ; i<5 ; i++) {
+            sum += tempNum%10;
+            tempNum /= 10;
+        }
+        
+        if (x%sum == 0) {
+            answer = true;
+        } else {
+            answer = false;
+        }
+        
+        return answer;
+    }
+}
