d1aa95f2abb730523b644801a4cf85264bb2ad0e
Newbiekk <newbiekk1126@gmail.com>
2024-11-04 09:47:05 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 31.82 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -2,9 +2,9 @@ class Solution {
     public double solution(double n) {
         double answer = 0;
         
-        for (double i = 1; i <= n ; i++) {
-            if (n/i == i) {
-                answer = (i+1) * (i+1);
+        for (double x = 1; x <= n ; x++) {
+            if (n/x == x) {
+                answer = (x+1) * (x+1);
                 break;
             } else {
                 answer = -1;
