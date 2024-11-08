a09ff913857a0c50dee59ebaab2b32686638c824
Newbiekk <newbiekk1126@gmail.com>
2024-11-08 16:54:34 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 10.57 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -3,15 +3,14 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
-        List<Integer> a = new ArrayList<>();
-        
+        int a = 0;
         for (int i = 1 ; i <= n ; i++) {
             if (n%i == 0 && m%i == 0 ) {
-                a.add(i);
+                a = i;
             }
         }
         
-        answer[0] = a.get(a.size()-1);
+        answer[0] = a;
         
         for (long i = 1 ; i <= n*m ; i++) {
             if (i%n == 0 && i%m == 0) {
