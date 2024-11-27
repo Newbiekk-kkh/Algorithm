477df40233a6844e32fe71c8e783095b4240d235
Newbiekk <newbiekk1126@gmail.com>
2024-11-27 10:31:18 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.11 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -4,7 +4,7 @@ class Solution {
         String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         
         for (int i = 0; i < numbers.length; i++) {
-            s = s.replaceAll(numbers[i], String.valueOf(i));
+            s = s.replace(numbers[i], String.valueOf(i));
         }
         
         answer = Integer.parseInt(s);
