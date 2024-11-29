2c5920379dc87e5fcb32ec70871be2d526d89167
Newbiekk <newbiekk1126@gmail.com>
2024-11-29 14:39:13 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 22.43 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -2,34 +2,12 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-        List<Integer> list = new ArrayList<>();
-        int cal = 1;
+        String answer = "0";
         
-        for (int i = 1; i<food.length; i++) {
-            for (int j = 0; j < food[i]/2; j++) {
-                list.add(cal);
-            }
-            cal++;
+        for (int i=1 ; i<food.length ; i++) {
+            for (int j = 0; j < food[food.length-i]/2; j++)
+            answer = String.valueOf(food.length-i) + answer + String.valueOf(food.length-i);
         }
-        
-        list.add(0);
-        cal -= 1;
-        
-        for (int i = food.length-1; i > 0; i--) {
-            for (int j = 0; j < food[i]/2; j++) {
-                list.add(cal);
-            }
-            cal--;
-        }
-        
-        String[] str = new String[list.size()];
-        
-        for (int i = 0; i<list.size(); i++) {
-            str[i] = Integer.toString(list.get(i));
-        }
-        
-        answer = String.join("",str);
         
         return answer;
     }
