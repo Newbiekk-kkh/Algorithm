165c58f8234f4a6d3aec5171775d955671be149b
Newbiekk <newbiekk1126@gmail.com>
2025-03-06 23:43:56 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        int general = hp / 5;
+        int soldier = (hp % 5) / 3;
+        int worker = (hp % 5) % 3;
+        answer = general + soldier + worker;
+        return answer;
+    }
+}
