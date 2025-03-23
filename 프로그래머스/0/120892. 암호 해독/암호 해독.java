d6552cd3c460ca15ab8104a2d105d08398c388bf
Newbiekk <newbiekk1126@gmail.com>
2025-03-23 23:44:27 +0900

    [level 0] Title: 암호 해독, Time: 0.14 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < cipher.length(); i++) {
+            if (i % code == code - 1) {
+                sb.append(cipher.charAt(i));
+            }
+        }
+        
+        answer = sb.toString();
+        
+        return answer;
+    }
+}
