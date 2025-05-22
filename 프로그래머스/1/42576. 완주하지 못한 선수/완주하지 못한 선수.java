8379a323c57f769feaaa0d6b71c587cef23c69ef
Newbiekk <newbiekk1126@gmail.com>
2025-05-22 21:01:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.40 ms, Memory: 97.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        Map<String, Integer> nameMap = new HashMap<>();
+        
+        for (String name : participant) {
+            nameMap.put(name, nameMap.getOrDefault(name, 0) + 1);
+        }
+        
+        for (String name : completion) {
+            nameMap.put(name, nameMap.get(name) - 1);
+        }
+        
+        for (String name : participant) {
+            if (nameMap.get(name) != 0) {
+                return name;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
